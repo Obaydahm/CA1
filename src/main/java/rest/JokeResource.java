8fa119351ba5339ef49453982cadcb97ca01b795
Obaydah <obaydahmp@hotmail.com>
2019-09-11 22:52:01 +0200

    finished frontend for joke

--- src/main/java/rest/JokeResource.java
+++ src/main/java/rest/JokeResource.java
@@ -1,14 +1,12 @@
 package rest;
 
+import DTO.JokeDTO;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import utils.EMF_Creator;
 import facades.JokeFacade;
 import javax.persistence.EntityManagerFactory;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
---
+++ src/main/webapp/scriptjoke.js
@@ -0,0 +1,39 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+let table = document.getElementById("table");
+let url = "https://localhost:8080/CA1/api/joke/all";
+function showTable(jokes) {
+    let tablehead = '<col width="5%"><col width="25%"><col width="70%"><thead><tr><th>#</th><th>Type</th><th>Joke</th></tr></thead><tbody>';
+    let tabledata = jokes.map(obj => "<tr><td>" + obj.id + "</td><td>" + obj.type + "</td><td>" + obj.joke + "</td>");
+    let tab = tablehead.concat(tabledata.join('') + "</tbody>");
+    table.innerHTML = tab;
+}
+
+fetch(url)
+.then(res => res.json()) //in flow1, just do it
+.then(data => {
+    // Inside this callback, and only here, the response data is available
+    window.onload = showTable(data);
+});
+
+function getById(id){
+	fetch("https://localhost:8080/CA1/api/joke/" + id)
+	.then(res => res.json()) //in flow1, just do it
+	.then(data => {
+		document.getElementById("jokeinfo").innerHTML = "<b>ID:</b> " + data.id + "<br> <b>Type:</b> " + data.type + "<br>" + data.joke;
+	    console.log(data)
+	});
+}
+
+
+function getRandomJoke(){
+	fetch("https://localhost:8080/CA1/api/joke/random")
+	.then(res => res.json()) //in flow1, just do it
+	.then(data => {
+		document.getElementById("randomjoke").innerHTML = "<b>ID:</b> " + data.id + "<br> <b>Type:</b> " + data.type + "<br>" + data.joke;
+	    console.log(data)
+	});
+}
