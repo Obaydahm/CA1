2af73f022598de3b4b377688955c4c88493b72a7
Obaydah <obaydahmp@hotmail.com>
2019-09-15 23:04:09 +0200

    fixed build error

--- src/test/java/rest/JokeResourceTest.java
+++ src/test/java/rest/JokeResourceTest.java
@@ -1,7 +1,6 @@
 package rest;
 
-import entities.Colour;
-import entities.Members;
+import entities.Joke;
 import utils.EMF_Creator;
 import io.restassured.RestAssured;
 import static io.restassured.RestAssured.given;
@@ -14,10 +13,7 @@ import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.grizzly.http.util.HttpStatus;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
-import org.hamcrest.Matchers;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasItem;
-import static org.hamcrest.Matchers.is;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -28,7 +24,7 @@ import utils.EMF_Creator.Strategy;
 
 //Uncomment the line below, to temporarily disable this test
 //@Disabled
-public class MembersResourceTest {
+public class JokeResourceTest {
 
     private static final int SERVER_PORT = 7777;
     private static final String SERVER_URL = "http://localhost/api";
@@ -39,7 +35,10 @@ public class MembersResourceTest {
     private static HttpServer httpServer;
     private static EntityManagerFactory emf;
     
-
+    Joke j = new Joke("Hvad sagde koen til kyllingen?: muuuuuuuuuuuuuuuuuh", "Ko og Kylling", "Cartoonnetwork");
+    Joke j2 = new Joke("Hvad sagde Pickachu til Ash: Picka Picka", "Pokemon", "Cartoonnetwork");
+    Joke j3 = new Joke("Joke3", "Kategori3", "google");
+            
     static HttpServer startServer() {
         ResourceConfig rc = ResourceConfig.forApplication(new ApplicationConfig());
         return GrizzlyHttpServerFactory.createHttpServer(BASE_URI, rc);
@@ -47,7 +46,7 @@ public class MembersResourceTest {
 
     @BeforeAll
     public static void setUpClass() {
-        emf = EMF_Creator.createEntityManagerFactory(DbSelector.TEST, Strategy.DROP_AND_CREATE);
+        emf = EMF_Creator.createEntityManagerFactory(DbSelector.TEST, Strategy.CREATE);
 
         //NOT Required if you use the version of EMF_Creator.createEntityManagerFactory used above        
         //System.setProperty("IS_TEST", TEST_DB);
@@ -75,58 +74,98 @@ public class MembersResourceTest {
         EntityManager em = emf.createEntityManager();
         try {
             em.getTransaction().begin();
-            em.createNamedQuery("Members.deleteAllRows").executeUpdate();
-            em.persist(new Members("Tom", "Tom@cphbusiness.dk", Colour.GREEN));
-            em.persist(new Members("Lone", "Lone@cphbusiness.dk", Colour.YELLOW));
-            em.persist(new Members("Sigurd", "Sigurd@cphbusiness.dk", Colour.RED));
+            em.createNamedQuery("Joke.deleteAllRows").executeUpdate();
+            em.persist(j);
+            em.getTransaction().commit();
             
-           
+            em.getTransaction().begin();
+            em.persist(j2);
+            em.getTransaction().commit();
+            
+            em.getTransaction().begin();
+            em.persist(j3);
             em.getTransaction().commit();
         } finally {
             em.close();
         }
     }
     
+    
     @Test
     public void testServerIsUp() {
         System.out.println("Testing is server UP");
-        given().when().get("/groupmembers").then().statusCode(200);
+        given().when().get("/joke").then().statusCode(200);
     }
    
     //This test assumes the database contains two rows
+    
     @Test
     public void testDummyMsg() throws Exception {
         given()
         .contentType("application/json")
-        .get("/groupmembers/").then()
+        .get("/joke/").then()
         .assertThat()
         .statusCode(HttpStatus.OK_200.getStatusCode())
         .body("msg", equalTo("Hello World"));   
     }
     
     @Test
-    public void testCount() throws Exception {
+    public void testGetAll() throws Exception {
         given()
         .contentType("application/json")
-        .get("/groupmembers/count").then()
+        .get("/joke/all").then()
         .assertThat()
         .statusCode(HttpStatus.OK_200.getStatusCode())
-        .body("count", equalTo(3));   
+        .log().body()
+        .body("[0].joke", equalTo("Hvad sagde koen til kyllingen?: muuuuuuuuuuuuuuuuuh"))
+        .and()
+        .body("[1].type", equalTo("Pokemon"))
+        .and()  
+        .body("[2].type", equalTo("Kategori3"))
+        .and()
+        .body("size()", equalTo(3));
+        
     }
     
     @Test
-    public void testGetListOfMembers()
-    {
+    public void testGetById() throws Exception {
+        //Joke 1
         given()
         .contentType("application/json")
-        .get("/groupmembers/all").then()
+        .get("/joke/" + j.getId()).then()
         .assertThat()
         .statusCode(HttpStatus.OK_200.getStatusCode())
-        .body("size()", is(3))
-        .and().body("name", hasItem("Tom"))
-        .and().body("name", hasItem("Lone"))
-        .and().body("name", hasItem("Sigurd"));
+        .log().body()
+        .body("id", equalTo(j.getId().intValue()))
+        .and()
+        .body("joke", equalTo(j.getJoke()))
+        .and()  
+        .body("type", equalTo(j.getType()));
+        
+        //Joke 2
+        given()
+        .contentType("application/json")
+        .get("/joke/" + j2.getId()).then()
+        .assertThat()
+        .statusCode(HttpStatus.OK_200.getStatusCode())
+        .log().body()
+        .body("id", equalTo(j2.getId().intValue()))
+        .and()
+        .body("joke", equalTo(j2.getJoke()))
+        .and()  
+        .body("type", equalTo(j2.getType()));
+        
+        //Joke 3
+        given()
+        .contentType("application/json")
+        .get("/joke/" + j3.getId()).then()
+        .assertThat()
+        .statusCode(HttpStatus.OK_200.getStatusCode())
+        .log().body()
+        .body("id", equalTo(j3.getId().intValue()))
+        .and()
+        .body("joke", equalTo(j3.getJoke()))
+        .and()  
+        .body("type", equalTo(j3.getType()));
     }
-    
-    
 }
